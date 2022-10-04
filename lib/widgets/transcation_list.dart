bdfaa652c53d1eb6d00fbf3a5a66f3e0ef0c5f7f
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-10-04 21:22:03 +0530

    Udemy

--- lib/widgets/transcation_list.dart
+++ lib/widgets/transcation_list.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../models/transcation.dart';
-
+//helloo
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
   final Function deleteTx;
